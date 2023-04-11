2e67f4032e9d6b56385f2ed67aa2ca28a87c26fd
Jean <jean.melo.it@outlook.com>
2023-04-11 13:07:04 -0300

    .

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 4000;
 
 const personRoutes = require("../routes/person-route");
 
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/person", personRoutes);
+
+app.get("/", function (req, res) {
+  res.status(200).send("Working!");
+});
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
